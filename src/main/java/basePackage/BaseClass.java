ca76e7d08f2d9c99c1703bab8d49d6dbcb7f1777
eshravani <shravanie.iiitb@gmail.com>
2024-10-29 19:17:36 -0500

    removed unnecessary prints

--- src/main/java/basePackage/BaseClass.java
+++ src/main/java/basePackage/BaseClass.java
@@ -36,14 +36,14 @@ public class BaseClass {
         System.setProperty("appName", appName);
         System.setProperty("browserName", browserName);
         url = getPropValue("URL");
-        System.out.println("In before class"+envType+"_"+envName+"_"+appName+"_"+browserName);
+        //System.out.println("In before class"+envType+"_"+envName+"_"+appName+"_"+browserName);
     }
        // String url= getPropInstance("URL");
         // Initialize driver using ThreadLocal instance from DriverInit
        @BeforeMethod
        public void setUpDriverConfig(){
-           System.out.println("In before mehod"+System.getProperty("envType")+"_"+System.getProperty("envName")+"_"+System.getProperty("appName")+"_"+System.getProperty("browserName"));
-           System.out.println("url is in before method"+url);
+         //  System.out.println("In before mehod"+System.getProperty("envType")+"_"+System.getProperty("envName")+"_"+System.getProperty("appName")+"_"+System.getProperty("browserName"));
+           //System.out.println("url is in before method"+url);
       //  String browserName=System.getProperty("browserName");
        // DriverInit.getInstance().setDriver(driver);
         WebDriver driver = DriverInit.getInstance().getDriver();
